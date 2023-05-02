68d9196a25beae9e7c4f070c6abadcbc94e03b78
Christian Bernard <ianbernard12@yahoo.com>
2023-05-03 01:31:15 +0700

    fixing cors config

--- src/main/java/com/paypay/config/CorsConfig.java
+++ src/main/java/com/paypay/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:8080/*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
     }
 
 }
